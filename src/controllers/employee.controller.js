c6703388b85a917e3a06f8919b427c110f665abc
Tal Koosh <tal@startupbooster.dev>
2022-09-20 17:22:07 +0300

    finished controllers & routes

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -1,7 +1,23 @@
 const employeeService = require("../services/employee.service");
 
-const getEmployees = async (req, res) => {};
+const getEmployees = async (req, res) => {
+  try {
+    const { filter } = req.body;
+    const employees = await employeeService.getEmployees(filter);
+    res.send(employees);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
-const updateEmployee = async (req, res) => {};
+const updateEmployee = async (req, res) => {
+  try {
+    const { updatedEmployee } = req.body;
+    const employee = await employeeService.updateEmployee(updatedEmployee);
+    res.send(employee);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
 module.exports = { getEmployees, updateEmployee };
--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,10 +1,34 @@
 const projectService = require("../services/project.service");
 
-const getProjects = async (req, res) => {};
+const getProjects = async (req, res) => {
+  try {
+    const { filter } = req.body;
+    const projects = await projectService.getProject(filter);
+    res.send(projects);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
-const addProject = async (req, res) => {};
+const addProject = async (req, res) => {
+  try {
+    const { newProject } = req.body;
+    const addedProject = await projectService.addProject(newProject);
+    res.send(addedProject);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
-const updateProject = async (req, res) => {};
+const updateProject = async (req, res) => {
+  try {
+    const { updatedProject } = req.body;
+    const project = await projectService.updateProject(updatedProject);
+    res.send(project);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
 module.exports = {
   getProjects,
